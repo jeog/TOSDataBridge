d4f361459292dfc84b09fcc7a1685ca2f45c5594
Jonathon <jeog.dev@gmail.com>
2015-03-28 12:26:16 -0400

    remove tabs in tosdb-setup.bat

--- tosdb-setup.bat
+++ tosdb-setup.bat
@@ -3,16 +3,16 @@
 NET FILE 1>NUL 2>NUL &
 
 IF %ERRORLEVEL% NEQ 0 (
-	echo tosdb-setup.bat must be run as administrator.
-	EXIT /B 1
+    echo tosdb-setup.bat must be run as administrator.
+    EXIT /B 1
 )
 
 set bCRTfiles=false
 
 IF EXIST C:/Windows/System32/msvcr110.dll (
-	IF EXIST C:/Windows/System32/msvcp110.dll (
-		set bCRTfiles=true
-	)
+    IF EXIST C:/Windows/System32/msvcp110.dll (
+        set bCRTfiles=true
+    )
 )
 
 set servCmd=""
@@ -20,62 +20,62 @@ set servCmd=""
 IF /I "%2"=="admin" set servCmd=" --admin"
 
 IF "%1"=="x64" (
-	echo + Moving _tos-databridge-shared-x64.dll to %WINDIR% ...
-	MOVE /-Y .\bin\Release\x64\_tos-databridge-shared-x64.dll %WINDIR%    
+    echo + Moving _tos-databridge-shared-x64.dll to %WINDIR% ...
+    MOVE /-Y .\bin\Release\x64\_tos-databridge-shared-x64.dll %WINDIR%    
     IF ERRORLEVEL 1 (
         echo - Move failed, exiting setup...
         EXIT /B 1
     )    
-	echo + Checking for VC++ Redistributable Files ...
-	IF %bCRTfiles%==false (		
-		echo ++ Not Found. Installing VC++ Redistributable ...
-		vcredist_x64.exe 
-		IF ERRORLEVEL 1 (
-			echo - Installation failed, exiting setup...
-			EXIT /B 1
-		)
-	)
-	echo + Creating TOSDataBridge Service ...
-	SC delete TOSDataBridge 1>NUL 2>NUL 
-	SC create TOSDataBridge binPath= %cd%\\bin\\Release\\x64\\tos-databridge-serv-x64.exe%servCmd%
-	IF ERRORLEVEL 1 (
-      echo - SC create failed, exiting setup...
-      EXIT /B 1
+    echo + Checking for VC++ Redistributable Files ...
+    IF %bCRTfiles%==false (        
+        echo ++ Not Found. Installing VC++ Redistributable ...
+        vcredist_x64.exe 
+        IF ERRORLEVEL 1 (
+            echo - Installation failed, exiting setup...
+            EXIT /B 1
+        )
+    )
+    echo + Creating TOSDataBridge Service ...
+    SC delete TOSDataBridge 1>NUL 2>NUL 
+    SC create TOSDataBridge binPath= %cd%\\bin\\Release\\x64\\tos-databridge-serv-x64.exe%servCmd%
+    IF ERRORLEVEL 1 (
+        echo - SC create failed, exiting setup...
+        EXIT /B 1
     )    
 ) else ( 
     IF "%1"=="x86" (
-		echo + Moving _tos-databridge-shared-x86.dll to %WINDIR% ...
-		MOVE /-Y .\bin\Release\Win32\_tos-databridge-shared-x86.dll %WINDIR%		
-	    IF ERRORLEVEL 1 (
+        echo + Moving _tos-databridge-shared-x86.dll to %WINDIR% ...
+        MOVE /-Y .\bin\Release\Win32\_tos-databridge-shared-x86.dll %WINDIR%        
+        IF ERRORLEVEL 1 (
             echo - Move failed, exiting setup...
             EXIT /B 1
         )    
-    	echo + Checking for VC++ Redistributable Files ...
-		IF EXIST C:/Windows/SysWOW64 set bCRTfiles=false
-		IF EXIST C:/Windows/SysWOW64/msvcr110.dll (
-			IF EXIST C:/Windows/SysWOW64/msvcp110.dll (
-				set bCRTfiles=true
-			)
-		)
-		IF %bCRTfiles%==false (		
-			echo ++ Not Found. Installing VC++ Redistributable ...
-			vcredist_x86.exe 
-		    IF ERRORLEVEL 1 (
-			    echo - Installation failed, exiting setup...
-			    EXIT /B 1
-		    )
-		)		
-		echo + Creating TOSDataBridge Service ...
-		SC delete TOSDataBridge 1>NUL 2>NUL 		
-		SC create TOSDataBridge binPath= %cd%\bin\Release\Win32\tos-databridge-serv-x86.exe%servCmd%
+        echo + Checking for VC++ Redistributable Files ...
+        IF EXIST C:/Windows/SysWOW64 set bCRTfiles=false
+        IF EXIST C:/Windows/SysWOW64/msvcr110.dll (
+            IF EXIST C:/Windows/SysWOW64/msvcp110.dll (
+                set bCRTfiles=true
+            )
+        )
+        IF %bCRTfiles%==false (        
+            echo ++ Not Found. Installing VC++ Redistributable ...
+            vcredist_x86.exe 
+            IF ERRORLEVEL 1 (
+                echo - Installation failed, exiting setup...
+                EXIT /B 1
+            )
+        )        
+        echo + Creating TOSDataBridge Service ...
+        SC delete TOSDataBridge 1>NUL 2>NUL         
+        SC create TOSDataBridge binPath= %cd%\bin\Release\Win32\tos-databridge-serv-x86.exe%servCmd%
         IF ERRORLEVEL 1 (
             echo - SC create failed, exiting setup...
             EXIT /B 1
         )    
-	) else (
-		echo - Invalid Command Line Argument. Use 'x86' or 'x64' to signify which build to setup.		
+    ) else (
+        echo - Invalid Command Line Argument. Use 'x86' or 'x64' to signify which build to setup.        
         EXIT /B 1
-		)
+        )
 )
 
 EXIT /B 0
