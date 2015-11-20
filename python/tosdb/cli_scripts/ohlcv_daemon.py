0891e7d14e0e3d57754b8ed160bf362d1919459f
Jonathon <jeog.dev@gmail.com>
2015-11-20 02:34:23 -0500

    tosdb_refactor cli_scripts

--- python/tosdb/cli_scripts/ohlcv_daemon.py
+++ python/tosdb/cli_scripts/ohlcv_daemon.py
@@ -1,9 +1,26 @@
+# Copyright (C) 2014 Jonathon Ogden   < jeog.dev@gmail.com >
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License,
+#   'LICENSE.txt', along with this program.  If not, see 
+#   <http://www.gnu.org/licenses/>.
+
 import tosdb
 from tosdb.intervalize import TimeInterval as _TI
+from .daemon import Daemon as _Daemon
+
 from argparse import ArgumentParser as _ArgumentParser
 from time import localtime as _localtime, strftime as _strftime, sleep as _sleep
 from os.path import realpath as _path
-from .daemon import Daemon as _Daemon
 from sys import stderr as _stderr
 
 _val_type = ''
@@ -13,82 +30,81 @@ AINIT_TIMEOUT = 5000
 BLOCK_SIZE = 1000
     
 class MyDaemon(_Daemon):
-    def __init__( self, addr, dll_root, out_dir, pid_file, error_file, intrvl, 
-                  symbols ):
-        _Daemon.__init__( self, pid_file, stderr = error_file)
-        self._addr = addr
-        self._dll_root = dll_root
-        self._out_dir = _path(out_dir)
-        self._intrvl = intrvl
-        self._symbols = symbols
-        self._iobjs = list()
+  def __init__(self, addr, dll_root, out_dir, pid_file, error_file, 
+               intrvl, symbols):
+    _Daemon.__init__(self, pid_file, stderr = error_file)
+    self._addr = addr
+    self._dll_root = dll_root
+    self._out_dir = _path(out_dir)
+    self._intrvl = intrvl
+    self._symbols = symbols
+    self._iobjs = list()
 
-    def run(self):  
-        # initialize windows side     
-        tosdb.admin_init( self._addr, AINIT_TIMEOUT )   
-        tosdb.vinit( root=self._dll_root )
+  def run(self):  
+    # initialize windows side   
+    tosdb.admin_init( self._addr, AINIT_TIMEOUT )   
+    tosdb.vinit( root=self._dll_root )
 
-        # create block
-        blk = tosdb.VTOSDB_DataBlock( self._addr, BLOCK_SIZE, date_time=True)
-        blk.add_items( *(self._symbols) )
-        blk.add_topics( 'last' )
-        if args.vol:
-            blk.add_topics( 'volume' )
+    # create block
+    blk = tosdb.VTOSDB_DataBlock( self._addr, BLOCK_SIZE, date_time=True)
+    blk.add_items( *(self._symbols) )
+    blk.add_topics( 'last' )
+    if args.vol:
+        blk.add_topics( 'volume' )
  
-        # generate filename                   
-        dprfx = _strftime("%Y%m%d", _localtime())
-        isec = int(self._intrvl * 60)  
-        for s in self._symbols:
-            #
-            # create GetOnTimeInterval object for each symbol
-            # 
-            p = self._out_dir + '/' + dprfx + '_' + \
-                    s.replace('/','-S-').replace('$','-D-').replace('.','-P-') + \
-                    '_' + _val_type + '_' + str(self._intrvl) + 'min.tosdb'           
-            iobj = _Goti.send_to_file( blk, s, p, _TI.vals[ isec ], isec/10)
-            print( repr(iobj) , file=_stderr )
-            self._iobjs.append( iobj )
+    # generate filename             
+    dprfx = _strftime("%Y%m%d", _localtime())
+    isec = int(self._intrvl * 60)  
+    for s in self._symbols:
+      #
+      # create GetOnTimeInterval object for each symbol
+      # 
+      p = self._out_dir + '/' + dprfx + '_' \
+          + s.replace('/','-S-').replace('$','-D-').replace('.','-P-') \
+          +'_' + _val_type + '_' + str(self._intrvl) + 'min.tosdb'           
+      iobj = _Goti.send_to_file(blk, s, p, _TI.vals[isec], isec/10)
+      print(repr(iobj), file=_stderr)
+      self._iobjs.append(iobj)
 
-        for i in self._iobjs:
-            if i:
-                i.join()
+    for i in self._iobjs:
+      if i:
+        i.join()
 
-        while(True):
-            _sleep(10)
+    while(True):
+      _sleep(10)
       
 
 if __name__ == '__main__':
-    parser = _ArgumentParser() 
-    parser.add_argument( 'addr', type=str, help = 'address of core(Windows) ' +
-                         'implementation " address port "' )
-    parser.add_argument( 'dllroot', type=str, help = 'root of the path of core'
-                         '(Windows) implementation DLL' )
-    parser.add_argument( 'outdir', type=str, help = 'directory to output ' +
-                         ' data/files to' )
-    parser.add_argument( 'pidfile', type=str, help = 'path of pid file' )
-    parser.add_argument( 'errorfile', type=str, help = 'path of error file' )
-    parser.add_argument( 'intrvl', type=int, 
-                         choices=tuple (map( lambda x: int(x/60), 
-                                             sorted(_TI.val_dict.keys()) ) ),
-                         help="interval size(minutes)" )
-    parser.add_argument( '--ohlc', action="store_true", 
-                         help="use open,high,low,close instead of close" )
-    parser.add_argument( '--vol', action="store_true", help="use volume")
-    parser.add_argument('vars', nargs='*', help="symbols to pull")
-    args = parser.parse_args()
+  parser = _ArgumentParser() 
+  parser.add_argument('addr', type=str, 
+                      help = 'address of Windows implementation "address port"')
+  parser.add_argument('dllroot', type=str, 
+                      help = 'root path of Windows implementation C Library')
+  parser.add_argument('outdir', type=str, help = 'directory to output data to')
+  parser.add_argument('pidfile', type=str, help = 'path of pid file')
+  parser.add_argument('errorfile', type=str, help = 'path of error file')
+  parser.add_argument('intrvl', type=int, 
+    choices=tuple(map(lambda x: int(x/60), sorted(_TI.val_dict.keys()))),
+                       help="interval size(minutes)" )
+  parser.add_argument('--ohlc', action="store_true", 
+                      help="use open/high/low/close instead of close")
+  parser.add_argument('--vol', action="store_true", help="use volume")
+  parser.add_argument('vars', nargs='*', help="symbols to pull")
+  args = parser.parse_args()
 
-    addr_parts = args.addr.split(' ')
-    addr = (addr_parts[0],int(addr_parts[1]))
-    
-    if args.ohlc:
-        _val_type = 'OHLCV' if args.vol else 'OHLC'
-    else:
-        _val_type = 'CV' if args.vol else 'C'
+  addr_parts = args.addr.split(' ')
+  addr = (addr_parts[0],int(addr_parts[1]))
+  
+  if args.ohlc:
+    _val_type = 'OHLCV' if args.vol else 'OHLC'
+  else:
+    _val_type = 'CV' if args.vol else 'C'
 
-    exec("from tosdb.intervalize import " + CLS_BASE + _val_type + " as _Goti")    
+  exc_cmd = "from tosdb.intervalize import " + CLS_BASE + _val_type + " as _Goti"
+  exec(exc_cmd)  
 
-    MyDaemon( addr, args.dllroot, args.outdir, args.pidfile, args.errorfile,
-              args.intrvl, args.vars).start()
+  MyDaemon(addr, args.dllroot, args.outdir, args.pidfile, args.errorfile,
+           args.intrvl, args.vars).start()
 
 
 
--- python/tosdb/cli_scripts/ohlcv_wintask.py
+++ python/tosdb/cli_scripts/ohlcv_wintask.py
@@ -1,5 +1,22 @@
+# Copyright (C) 2014 Jonathon Ogden   < jeog.dev@gmail.com >
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License,
+#   'LICENSE.txt', along with this program.  If not, see 
+#   <http://www.gnu.org/licenses/>.
+
 import tosdb
 from tosdb.intervalize import TimeInterval as _TI
+
 from argparse import ArgumentParser as _ArgumentParser
 from time import localtime as _localtime, strftime as _strftime, sleep as _sleep
 from os.path import realpath as _path
@@ -8,63 +25,61 @@ from sys import stderr as _stderr
 CLS_BASE = 'GetOnTimeInterval_'
 BLOCK_SIZE = 1000
 
-def spawn(dllroot,outdir,intrvl,val_type,*symbols):
-   
-    if val_type not in ['OHLCV','OHLC','CV','C']:
-         raise ValueError("invalid val_type (OHLCV,OHLC,CV or C)")
+def spawn(dllroot,outdir,intrvl,val_type,*symbols):   
+  if val_type not in ['OHLCV','OHLC','CV','C']:
+    raise ValueError("invalid val_type (OHLCV,OHLC,CV or C)")
 
-    exec( "from tosdb.intervalize import " + CLS_BASE + val_type + " as _Goti",
-          globals() )
-    
-    tosdb.init( root=dllroot )
-    
-    # create block
-    blk = tosdb.TOSDB_DataBlock( BLOCK_SIZE, date_time=True)
-    blk.add_items( *(symbols) )
-    blk.add_topics( 'last' )
-    if 'V' in val_type:
-        blk.add_topics( 'volume' )
-        
-    # generate filename                
-    dprfx = _strftime("%Y%m%d", _localtime())
-    isec = int(intrvl * 60)    
-    iobjs = list()
-    for s in symbols:
-        #
-        # create GetOnTimeInterval object for each symbol
-        # 
-        p = _path(outdir) + '/' + dprfx + '_' + \
-                s.replace('/','-S-').replace('$','-D-').replace('.','-P-') + \
-                '_' + val_type + '_' + str(intrvl) + 'min.tosdb'           
-        iobj = _Goti.send_to_file( blk, s, p, getattr(_TI,_TI.val_dict[ isec ]),
-                                   isec/10)
-        print( repr(iobj) )
-        iobjs.append( iobj )
-    
-    return iobjs
+  exc_cmd = "from tosdb.intervalize import " + CLS_BASE + val_type + " as _Goti"
+  exec(exc_cmd, globals())
+  
+  tosdb.init(root=dllroot)
+  
+  # create block
+  blk = tosdb.TOSDB_DataBlock(BLOCK_SIZE, date_time=True)
+  blk.add_items( *(symbols) )
+  blk.add_topics( 'last' )
+  if 'V' in val_type:
+    blk.add_topics( 'volume' )
+      
+  # generate filename          
+  dprfx = _strftime("%Y%m%d", _localtime())
+  isec = int(intrvl * 60)  
+  iobjs = list()
+
+  for s in symbols:
+    #
+    # create GetOnTimeInterval object for each symbol
+    # 
+    p = _path(outdir) + '/' + dprfx + '_' \
+        + s.replace('/','-S-').replace('$','-D-').replace('.','-P-') \
+        + '_' + val_type + '_' + str(intrvl) + 'min.tosdb'       
+    iobj = _Goti.send_to_file(blk, s, p, getattr(_TI,_TI.val_dict[isec]), isec/10)
+    print( repr(iobj) )
+    iobjs.append( iobj )
+  
+  return iobjs
+
 
 if __name__ == '__main__':
-    parser = _ArgumentParser()    
-    parser.add_argument( 'dllroot', type=str, help = 'root of the path of core'
-                         '(Windows) implementation DLL' )
-    parser.add_argument( 'outdir', type=str, help = 'directory to output ' +
-                         ' data/files to' )
-    parser.add_argument( 'intrvl', type=int, 
-                         choices=tuple( map( lambda x: int(x/60),
-                                             sorted(_TI.val_dict.keys()) ) ),
-                         help="interval size(minutes)" )
-    parser.add_argument( '--ohlc', action="store_true", 
-                         help="use open,high,low,close instead of close" )
-    parser.add_argument( '--vol', action="store_true", help="use volume")
-    parser.add_argument('symbols', nargs='*', help="symbols to pull")
-    args = parser.parse_args()
+  parser = _ArgumentParser()  
+  parser.add_argument('dllroot', type=str,                       
+                      help = 'root path of Windows implementation C Library')
+  parser.add_argument('outdir', type=str, help='directory to output data to')
+  parser.add_argument('intrvl', type=int, 
+    choices=tuple(map(lambda x: int(x/60), sorted(_TI.val_dict.keys()))),
+                      help="interval size(minutes)")
+  parser.add_argument('--ohlc', action="store_true", 
+                      help="use open/high/low/close instead of close")
+  parser.add_argument('--vol', action="store_true", help="use volume")
+  parser.add_argument('symbols', nargs='*', help="symbols to pull")
+  args = parser.parse_args()
 
-    if args.ohlc:
-        val_type = 'OHLCV' if args.vol else 'OHLC'
-    else:
-        val_type = 'CV' if args.vol else 'C'
+  if args.ohlc:
+    val_type = 'OHLCV' if args.vol else 'OHLC'
+  else:
+    val_type = 'CV' if args.vol else 'C'
 
-    spawn(args.dllroot, args.outdir, args.intrvl, val_type, *arg.vars)
+  spawn(args.dllroot, args.outdir, args.intrvl, val_type, *arg.vars)
 
 
 
