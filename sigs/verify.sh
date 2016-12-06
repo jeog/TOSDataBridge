29a18d6c8d5a1a79ac6ed15833cb83d3d99a992d
Jonathon <jeog.dev@gmail.com>
2016-12-05 20:39:51 -0500

    update sigs/verify.sh

--- sigs/verify.sh
+++ sigs/verify.sh
@@ -3,50 +3,117 @@
 echo "verify.sh assumes: 
   1) you have a working gpg in your path
   2) you've imported jeog.dev.key 
-  3) you've checked the hashes of all the relevant files(see notes_checksums.txt)
 "
 
-SIGS_DIR=$( cd "$( dirname "$0" )" && pwd )
+sigs_dir=$( cd "$( dirname "$0" )" && pwd )
 
-cd $SIGS_DIR
+cd $sigs_dir
 
-X86_SIG_FILES=($(ls | egrep '.*tos-databridge.+x86\..+asc'))
-X64_SIG_FILES=($(ls | egrep '.*tos-databridge.+x64\..+asc'))
+x86_sig_files=($(ls | egrep '.*tos-databridge.+x86\..+asc'))
+x64_sig_files=($(ls | egrep '.*tos-databridge.+x64\..+asc'))
 
-_good_or_bad(){
-  if (( $1 )); then
-    VBAD=$(($VBAD+1))
-  else
-    VGOOD=$(($VGOOD+1))
-  fi  
+check_return=0
+
+cmp_checksums()
+{
+    echo '   ' $1 'sha256 checksums'
+    off=$(cat notes_checksums.txt | grep ' '$1'$' | cut -d' ' -f1) 
+    calc=$(sha256sum $2 | cut -d' ' -f1)
+    echo '      ' $off 
+    echo '      ' $calc        
+    [[ $off == $calc ]]
+    check_return=$?
 }
 
-VGOOD=0
-VBAD=0
-for f in ${X86_SIG_FILES[*]}; do
-  echo "::: $f :::"
-  gpg --verify $f "../bin/Release/Win32/${f%.asc}";
-  echo ""
-  _good_or_bad $?
-done
-X86_SUMMARY="x86 binaries: $VGOOD good, $VBAD bad"
+run_checks()
+{
+    files=("${!1}")
+    for sig in ${files[*]}; do
+        echo "::: ${sig%.asc} :::"
 
-VGOOD=0
-VBAD=0
-for f in ${X64_SIG_FILES[*]}; do
-  echo "::: $f :::"
-  gpg --verify $f "../bin/Release/x64/${f%.asc}";
-  echo ""
-  _good_or_bad $?
-done
-X64_SUMMARY="x64 binaries: $VGOOD good, $VBAD bad"
+        # signature checksums
+        cmp_checksums $sig $sig
+        if (( $check_return )); then
+            vbad_checksum_sig=$(($vbad_checksum_sig+1))
+        else
+            vgood_checksum_sig=$(($vgood_checksum_sig+1))
+        fi  
+
+        # binary checksums
+        bin="${sig%.asc}"
+        binpath=$2$bin 
+        cmp_checksums $bin $binpath 
+        if (( $check_return )); then
+            vbad_checksum_bin=$(($vbad_checksum_bin+1))
+        else
+            vgood_checksum_bin=$(($vgood_checksum_bin+1))
+        fi  
+
+        # verify
+        gpg --verify $sig $binpath;       
+        if (( $? )); then
+            vbad=$(($vbad+1))
+        else
+            vgood=$(($vgood+1))
+        fi 
+        echo ""
+    done
+}
 
 
-echo ""
-echo "::: SUMMARY :::"
-echo "$X86_SUMMARY"
-echo "$X64_SUMMARY"
-echo ":::::::::::::::"
-echo ""
+vgood=0
+vbad=0
+vgood_checksum_sig=0
+vbad_checksum_sig=0
+vgood_checksum_bin=0
+vbad_checksum_bin=0
+
+
+run_checks x86_sig_files[@] "../bin/Release/Win32/"
+
+x86_summary="x86 binaries: $vgood good, $vbad bad"
+x86_summary_checksum_sig="x86 binaries: $vgood_checksum_sig good, $vbad_checksum_sig bad"
+x86_summary_checksum_bin="x86 binaries: $vgood_checksum_bin good, $vbad_checksum_bin bad"
+
+bad_total=$(($vbad + $vbad_checksum_sig + $vbad_checksum_bin))
+
+
+vgood=0
+vbad=0
+vgood_checksum_sig=0
+vbad_checksum_sig=0
+vgood_checksum_bin=0
+vbad_checksum_bin=0
+
+run_checks x64_sig_files[@] "../bin/Release/x64/"
+
+x64_summary="x64 binaries: $vgood good, $vbad bad"
+x64_summary_checksum_sig="x64 binaries: $vgood_checksum_sig good, $vbad_checksum_sig bad"
+x64_summary_checksum_bin="x64 binaries: $vgood_checksum_bin good, $vbad_checksum_bin bad"
+
+
+bad_total=$(($bad_total + $vbad + $vbad_checksum_sig + $vbad_checksum_bin))
+
+echo 
+if (( $bad_total )); then    
+    echo "*** $bad_total ERRORS ***"
+else
+    echo "*** SUCCESS ***"
+fi
+echo
+
+echo 
+echo "::: summary (sig checksums) :::"
+echo "$x86_summary_checksum_sig"
+echo "$x64_summary_checksum_sig"
+echo 
+echo "::: summary (bin checksums) :::"
+echo "$x86_summary_checksum_bin"
+echo "$x64_summary_checksum_bin"
+echo 
+echo "::: summary (signatures) :::"
+echo "$x86_summary"
+echo "$x64_summary"
+echo
 
 
