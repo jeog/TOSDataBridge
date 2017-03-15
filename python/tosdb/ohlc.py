270331ff8e077901a6ccf598d66165ceccbf32b9
jeog <jeog.dev@gmail.com>
2017-03-15 13:42:54 -0400

    ohlc.py (2)

--- python/tosdb/ohlc.py
+++ python/tosdb/ohlc.py
@@ -25,7 +25,11 @@ from .doxtend import doxtend as _doxtend
 from threading import Thread as _Thread, Lock as _Lock
 from types import MethodType as _MethodType
 from collections import deque as _Deque
-from concurrent import futures
+from concurrent import futures as _futures
+from time import time as _time, sleep as _sleep
+from itertools import groupby as _groupby
+
+import traceback as _traceback
 
 class OHLC:
     def __init__(self, o, h, l, c, epoch_intervals):
@@ -41,7 +45,7 @@ class TOSDB_OpenHighLowClose:
     MIN_INTERVAL_SEC = 10
     MAX_INTERVAL_SEC = 60 * 60 * 4 # 4 hours
     BLOCK_SIZE_PER_PSEC = 10
-    ALLOWED_TOPICS = [TOPICS.LAST, TOPICS.VOLUME]
+    ALLOWED_TOPICS = [TOPICS.LAST.val, TOPICS.VOLUME.val]
     BLOCK_ATTR = ['is_thread_safe', 'stream_snapshot_from_marker', 'topics']
     
     def __init__(self, block, interval_sec, poll_sec=.5):
@@ -53,47 +57,55 @@ class TOSDB_OpenHighLowClose:
         self._restrict_block_resize(block, poll_sec)
         self._rflag = False
         self._buffers = dict()
-        self._buffers_lock = Lock()
+        self._buffers_lock = _Lock()
         self._bthread = _Thread(target=self._background_worker)
         self._bthread.start()      
 
+    def stop(self):
+        self._rflag = False
+
+    def __del__(self):
+        self.stop()
 
     def _background_worker(self): # can we pass a method to submit ?       
         self._rflag = True
         while self._rflag:
             tbeg = _time()
-            self._manage_buffers()
-            for (t,i),b in self._buffers:
-                dat = self._block.stream_snapshot_from_marker(i,t,date_time=True)
-                if not dat:
-                    pass # check if we jumped the interval
-                         # maintain a count of continues bad dats
-                         # check if it > ._isec / ._psec
-                         # if so add an empy ohlc
-                self._parse_data(dat)    
+            try:
+                self._manage_buffers()
+                for (t,i),buf in self._buffers.items():
+                    dat = self._block.stream_snapshot_from_marker(i,t,date_time=True,
+                                                                  throw_if_data_lost=False)
+                    if not dat:
+                        continue
+                             # check if we jumped the interval
+                             # maintain a count of continues bad dats
+                             # check if it > ._isec / ._psec
+                             # if so add an empy ohlc
+                    self._parse_data(t,i,dat)
+            except Exception as e:
+                print("exc in _background_worker:", e)
+                _traceback.print_exc()
             tend = _time()
             _sleep( max(self._psec - (tend - tbeg), 0) )
-            ### DEBUG
-            return
-            ### DEBUG
-
+         
 
     def _parse_data(self, topic, item, dat):
         ei = self._buffers[(topic,item)][0].epoch_intervals
         fi =  len(dat)
         for d in dat:
-            d0 = int(d[1].mktime) % self._isec                    
+            d0 = int(d[1].mktime // self._isec)                    
             if d0 == ei:
                 break
             fi -= 1
         ndat = dat[fi:]
         odat = dat[:fi]
         if odat:
-            self._update_ohlc(t,i,[d[0] for d in odat], ei)
+            self._update_ohlc(topic,item,[d[0] for d in odat], ei)
         if ndat:
             pos = len(dat) - fi -1
-            ei = int(dat[pos].mktime) % self._isec
-            self._insert_ohlc(t, i, [d[0] for d in ndat], ei)
+            ei = int(dat[pos][1].mktime // self._isec)
+            self._insert_ohlc(topic,item, [d[0] for d in ndat], ei)
 
             
     def _update_ohlc(self, topic, item, dat, epcoh_intervals):        
@@ -101,7 +113,7 @@ class TOSDB_OpenHighLowClose:
         ohlc.h = max(ohlc.h, max(dat))
         ohlc.l = min(ohlc.l, min(dat))
         ohlc.c = dat[-1]
-        ohlc.epcoh_intervals = epcoh_intervals
+        ohlc.epoch_intervals = epcoh_intervals
         
 
     def _insert_ohlc(self, topic, item, dat, epcoh_intervals):
@@ -115,23 +127,31 @@ class TOSDB_OpenHighLowClose:
         self._buffers.clear()
         items = self._block.items()
         for t in self._block.topics():
-            if TOPICS.val_dict[t] not in cls.ALLOWED_TOPICS:
+            if TOPICS.val_dict[t] not in TOSDB_OpenHighLowClose.ALLOWED_TOPICS:
                   continue                  
-            for i in times:
+            for i in items:
                 if (t,i) not in old_buffers:
                     new_buffer_ids.append((t,i))
                 self._buffers[(t,i)] = old_buffers.get((t,i),_Deque())
-        with _futures.ThreadPoolExecutor(max_workers = len(new_buffer_ids)) as e:            
-            futs = {e.submit(self._init_buffer,self,t,i):(t,i) for (t,i),b in new_buffer_ids}
+        w = len(new_buffer_ids)
+        if w == 0:
+            return        
+        with _futures.ThreadPoolExecutor(max_workers = w) as e:            
+            futs = {e.submit(self._init_buffer,t,i):(t,i) for t,i in new_buffer_ids}
             for f in _futures.as_completed(futs):
                 t, i = futs[f]                
-                dat = f.result()           
-                self._parse_data(t, i, dat)
+                dat = f.result()                
+                # does groupby guarantee sorted ??
+                for e, grp in _groupby(dat, lambda t: int(t[1].mktime // self._isec)):
+                    d = [i[0] for i in grp]
+                    ohlc = OHLC(d[0], max(d), min(d), d[-1], e)        
+                    self._buffers[(t,i)].append(ohlc)                    
 
 
     def _init_buffer(self, topic, item):
-        for _ in range(self._isec / self._psec):
-            dat = self._block.stream_snapshot_from_marker(i,t,date_time=True)
+        for _ in range(int(self._isec / self._psec)):
+            dat = self._block.stream_snapshot_from_marker(item,topic,date_time=True,
+                                                          throw_if_data_lost=False)
             if dat:
                 return dat
         raise TOSDB_Error("failed to get any data for first interval: %s, %s" % (topic,item))
