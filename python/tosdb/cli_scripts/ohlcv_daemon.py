98e7cc986245b5ee8707a2c839999bdbb05b97ba
jeog <jeog.dev@gmail.com>
2017-03-24 13:08:08 -0400

    start converting cli_scripts to ohlc

--- python/tosdb/cli_scripts/ohlcv_daemon.py
+++ python/tosdb/cli_scripts/ohlcv_daemon.py
@@ -15,7 +15,7 @@
 #   <http://www.gnu.org/licenses/>.
 
 import tosdb
-from tosdb.intervalize import TimeInterval as _TI
+from tosdb import ohlc
 from .daemon import Daemon as _Daemon
 
 from argparse import ArgumentParser as _ArgumentParser
@@ -25,24 +25,73 @@ from sys import stderr as _stderr
 
 _val_type = ''
 
-CLS_BASE = 'GetOnTimeInterval_'
 AINIT_TIMEOUT = 5000
 BLOCK_SIZE = 1000
-
     
 class MyDaemon(_Daemon):
     def __init__(self, addr, dll_root, out_dir, pid_file, error_file, 
-                 intrvl, symbols):
+                 interval, symbols):
         _Daemon.__init__(self, pid_file, stderr = error_file)
         self._addr = addr
         self._dll_root = dll_root
         self._out_dir = _path(out_dir)
-        self._intrvl = intrvl
-        self._symbols = symbols
-        self._iobjs = list()
+        self._interval = interval
+        self._symbols = symbols        
 
 
-    def run(self):  
+    def run(self):
+        # generate filename          
+        dprfx = _strftime("%Y%m%d", _localtime())
+         
+        paths = {s.upper() : (_path(outdir) + '/' + dprfx + '_' \
+                             + s.replace('/','-S-').replace('$','-D-').replace('.','-P-') \
+                             + '_' + _val_type + '_' + str(self._interval) + 'min.tosdb') \
+                 for s in self._symbols}
+
+        # open file to write
+        files = {s : open(s,'w',1) for s in paths}
+
+        def callback_c(item, topic, iobj):
+            f = files[item]        
+            f.write(iobj.asctime().ljust(50))
+            if not iobj.is_null():
+                f.write(str(iobj.c) + '\n')
+
+        def callback_ohlc(item, topic, iobj):
+            f = files[item]        
+            f.write(iobj.asctime().ljust(50))
+            if not iobj.is_null():
+                d = (iobj.open, iobj.high, iobj.low, iobj.close)
+                f.write(str(str(d)) + '\n')
+
+        class _callback_matcher
+            def __init__(self, attrs)
+                self.other_interval = dict()
+                self.attrs = attrs
+            def get(self, i):
+                return (getattr(i, v) for v in self.attrs)
+            def callback(self, item, topic, iobj):
+                if other_interval[item] is None:
+                    other_interval[item] = (topic, iobj)
+                else:
+                    if other_interval[item][0] == topic:
+                        raise TOSDB_Error("last/volume mismatch")
+                    if other_interval[item][1].intervals_since_epoch \
+                       != iobj.intervals_since_epoch:
+                        raise TOSDB_Error("last/volume interval mismatch")
+                    f = files[item]        
+                    f.write(iobj.asctime().ljust(50))
+                    if not iobj.is_null():                    
+                        d = (self.get(iobj), self.get(other_interval[item][1]))
+                        if topic == 'VOLUME':                    
+                            d = (d[1],d[0])
+                        d = tuple(d[0]) + tuple(d[1])
+                        f.write(str(d))
+                    other_interval[item].clear()
+
+        callback_cv = _callback_matcher('c')
+        callback_ohlcv = _callback_matcher('ohlc')
+    
         # initialize windows side   
         tosdb.admin_init(self._addr, AINIT_TIMEOUT)   
         tosdb.vinit(root=self._dll_root)
@@ -51,29 +100,22 @@ class MyDaemon(_Daemon):
         blk = tosdb.VTOSDB_DataBlock(self._addr, BLOCK_SIZE, date_time=True)
         blk.add_items(*(self._symbols))
         blk.add_topics('last')
-        if args.vol:
-            blk.add_topics('volume')
-     
-        # generate filename             
-        dprfx = _strftime("%Y%m%d", _localtime())
-        isec = int(self._intrvl * 60)  
-        for s in self._symbols:
-            #
-            # create GetOnTimeInterval object for each symbol
-            # 
-            p = self._out_dir + '/' + dprfx + '_' \
-                + s.replace('/','-S-').replace('$','-D-').replace('.','-P-') \
-                +'_' + _val_type + '_' + str(self._intrvl) + 'min.tosdb'           
-            iobj = _Goti.send_to_file(blk, s, p, _TI.vals[isec], isec/10)
-            print(repr(iobj), file=_stderr)
-            self._iobjs.append(iobj)
-
-        for i in self._iobjs:
-            if i:
-                i.join()
-
-        while(True): # why??
-            _sleep(10)
+    
+        # create interval object
+        if 'OHLC' in _val_type:
+            if 'V' in _val_type:
+                blk.add_topics('volume')
+                iobj = TOSDB_OpenHighLowCloseIntervals(blk, interval, interval_cb=callback_ohlcv)
+            else:
+                iobj = TOSDB_OpenHighLowCloseIntervals(blk, interval, interval_cb=callback_ohlc)
+        else:
+            if 'V' in _val_type:
+                blk.add_topics('volume')
+                iobj = TOSDB_CloseIntervals(blk, interval, interval_cb=callback_cv)
+            else:
+                iobj = TOSDB_CloseIntervals(blk, interval, interval_cb=callback_c)
+                
+        ### TODO: HANDLE WAIT/EXIT
       
 
 if __name__ == '__main__':
@@ -85,7 +127,7 @@ if __name__ == '__main__':
     parser.add_argument('outdir', type=str, help = 'directory to output data to')
     parser.add_argument('pidfile', type=str, help = 'path of pid file')
     parser.add_argument('errorfile', type=str, help = 'path of error file')
-    parser.add_argument('intrvl', type=int, 
+    parser.add_argument('interval', type=int, 
                         choices=tuple(map( lambda x: int(x/60),
                                            sorted(_TI.val_dict.keys()) )),
                         help="interval size(minutes)" )
@@ -103,11 +145,9 @@ if __name__ == '__main__':
     else:
         _val_type = 'CV' if args.vol else 'C'
 
-    exc_cmd = "from tosdb.intervalize import " + CLS_BASE + _val_type + " as _Goti"
-    exec(exc_cmd)  
 
     MyDaemon(addr, args.dllroot, args.outdir, args.pidfile, args.errorfile,
-             args.intrvl, args.vars).start()
+             args.interval, args.vars).start()
 
 
 
--- python/tosdb/cli_scripts/ohlcv_wintask.py
+++ python/tosdb/cli_scripts/ohlcv_wintask.py
@@ -15,61 +15,104 @@
 #   <http://www.gnu.org/licenses/>.
 
 import tosdb
-from tosdb.intervalize import TimeInterval as _TI
+#from tosdb.intervalize import TimeInterval as _TI
+from tosdb import ohlc
 
 from argparse import ArgumentParser as _ArgumentParser
 from time import localtime as _localtime, strftime as _strftime, sleep as _sleep
 from os.path import realpath as _path
 from sys import stderr as _stderr
 
-CLS_BASE = 'GetOnTimeInterval_'
+
 BLOCK_SIZE = 1000
-
-
-def spawn(dllroot,outdir,intrvl,val_type,*symbols):   
-    if val_type not in ['OHLCV','OHLC','CV','C']:
-        raise ValueError("invalid val_type (OHLCV,OHLC,CV or C)")
-
-    exc_cmd = "from tosdb.intervalize import " + CLS_BASE + val_type + " as _Goti"
-    exec(exc_cmd, globals())
     
+
+def spawn(dllroot,outdir,interval,val_type,*symbols):   
+    if val_type not in ['OHLCV','OHLC','CV','C']:
+        raise ValueError("invalid val_type (OHLCV,OHLC,CV or C)") 
+        
+    # generate filename          
+    dprfx = _strftime("%Y%m%d", _localtime())
+     
+    paths = {s.upper() : (_path(outdir) + '/' + dprfx + '_' \
+                         + s.replace('/','-S-').replace('$','-D-').replace('.','-P-') \
+                         + '_' + val_type + '_' + str(interval) + 'min.tosdb') for s in symbols}
+
+    # open file to write
+    files = {s : open(s,'w',1) for s in paths}
+    
+    def callback_c(item, topic, iobj):
+        f = files[item]        
+        f.write(iobj.asctime().ljust(50))
+        if not iobj.is_null():
+            f.write(str(iobj.c) + '\n')
+
+    def callback_ohlc(item, topic, iobj):
+        f = files[item]        
+        f.write(iobj.asctime().ljust(50))
+        if not iobj.is_null():
+            d = (iobj.open, iobj.high, iobj.low, iobj.close)
+            f.write(str(str(d)) + '\n')
+
+    class _callback_matcher
+        def __init__(self, attrs)
+            self.other_interval = dict()
+            self.attrs = attrs
+        def get(self, i):
+            return (getattr(i, v) for v in self.attrs)
+        def callback(self, item, topic, iobj):
+            if other_interval[item] is None:
+                other_interval[item] = (topic, iobj)
+            else:
+                if other_interval[item][0] == topic:
+                    raise TOSDB_Error("last/volume mismatch")
+                if other_interval[item][1].intervals_since_epoch \
+                   != iobj.intervals_since_epoch:
+                    raise TOSDB_Error("last/volume interval mismatch")
+                f = files[item]        
+                f.write(iobj.asctime().ljust(50))
+                if not iobj.is_null():                    
+                    d = (self.get(iobj), self.get(other_interval[item][1]))
+                    if topic == 'VOLUME':                    
+                        d = (d[1],d[0])
+                    d = tuple(d[0]) + tuple(d[1])
+                    f.write(str(d))
+                other_interval[item].clear()
+
+    callback_cv = _callback_matcher('c')
+    callback_ohlcv = _callback_matcher('ohlc')
+  
+    # connect 
     tosdb.init(root=dllroot)
     
     # create block
     blk = tosdb.TOSDB_DataBlock(BLOCK_SIZE, date_time=True)
     blk.add_items(*(symbols))
-    blk.add_topics('last')
-    if 'V' in val_type:
-        blk.add_topics('volume')
+    blk.add_topics('last')        
         
-    # generate filename          
-    dprfx = _strftime("%Y%m%d", _localtime())
-    isec = int(intrvl * 60)  
-    iobjs = list()
+    # create interval object
+    if 'OHLC' in val_type:
+        if 'V' in val_type:
+            blk.add_topics('volume')
+            iobj = TOSDB_OpenHighLowCloseIntervals(blk, interval, interval_cb=callback_ohlcv)
+        else:
+            iobj = TOSDB_OpenHighLowCloseIntervals(blk, interval, interval_cb=callback_ohlc)
+    else:
+        if 'V' in val_type:
+            blk.add_topics('volume')
+            iobj = TOSDB_CloseIntervals(blk, interval, interval_cb=callback_cv)
+        else:
+            iobj = TOSDB_CloseIntervals(blk, interval, interval_cb=callback_c)
 
-    for s in symbols:
-        #
-        # create GetOnTimeInterval object for each symbol
-        # 
-        p = _path(outdir) + '/' + dprfx + '_' \
-            + s.replace('/','-S-').replace('$','-D-').replace('.','-P-') \
-            + '_' + val_type + '_' + str(intrvl) + 'min.tosdb'       
-        iobj = _Goti.send_to_file(blk, s, p, getattr(_TI,_TI.val_dict[isec]), isec/10)
-        print( repr(iobj) )
-        iobjs.append( iobj )
-    
-    return iobjs
-
+    ### TODO: HANDLE WAIT/EXIT
+            
 
 if __name__ == '__main__':
     parser = _ArgumentParser()  
     parser.add_argument('dllroot', type=str,                       
                         help = 'root path of Windows implementation C Library')
     parser.add_argument('outdir', type=str, help='directory to output data to')
-    parser.add_argument('intrvl', type=int, 
-                        choices=tuple(map( lambda x: int(x/60),
-                                           sorted(_TI.val_dict.keys()) )),
-                        help="interval size(minutes)")
+    parser.add_argument('interval', type=int, help="interval size in seconds")
     parser.add_argument('--ohlc', action="store_true", 
                         help="use open/high/low/close instead of close")
     parser.add_argument('--vol', action="store_true", help="use volume")
@@ -81,7 +124,7 @@ if __name__ == '__main__':
     else:
         val_type = 'CV' if args.vol else 'C'
 
-    spawn(args.dllroot, args.outdir, args.intrvl, val_type, *arg.vars)
+    spawn(args.dllroot, args.outdir, args.interval, val_type, *arg.vars)
 
 
 
